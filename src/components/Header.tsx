651f73dbd2c48ce3036ef2a0826a1500972bfdad
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-04-21 11:44:27 +0700

    feat: middle header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,28 @@
-import React, { Fragment } from 'react'
+import { Fragment } from 'react'
 import Image from 'next/image'
+import { PlusCircleIcon, UserGroupIcon, HeartIcon, PaperAirplaneIcon, MagnifyingGlassIcon, Bars3Icon } from '@heroicons/react/24/outline';
 
 const Header = () => {
     return (
         <Fragment>
             <div className='flex justify-between max-w-6xl'>
-                <div className='relative hidden lg:inline-grid h-24 w-24'>
+                <div className='relative hidden lg:inline-grid h-24 w-24 mt-3'>
                     <Image src='https://links.papareact.com/ocw' alt='full-logo-image' width={1000} height={1000} />
                 </div>
-            </div>
 
-            <div className='relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer'>
-                <Image src='https://links.papareact.com/jjm' alt='mini-logo-image' width={1000} height={1000} />
+                <div className='relative w-10 lg:hidden flex-shrink-0 cursor-pointer mt-3'>
+                    <Image src='https://links.papareact.com/jjm' alt='mini-logo-image' width={1000} height={1000} />
+                </div>
+
+                <div className='max-w-xs'>
+                    <div className='relative mt-1 p-3 rounded-md'>
+                        <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+                            <MagnifyingGlassIcon className='h-5 w-5 text-gray-500' />
+                        </div>
+                        <input className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md' type='text' placeholder='Search' />
+                    </div>
+                </div>
+
             </div>
         </Fragment>
     )
