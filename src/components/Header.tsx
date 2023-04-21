242e4e930871421c004105f7f104b052e37240f3
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-04-21 12:43:32 +0700

    feat: right header component and sticky header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,44 @@
 import { Fragment } from 'react'
 import Image from 'next/image'
 import { PlusCircleIcon, UserGroupIcon, HeartIcon, PaperAirplaneIcon, MagnifyingGlassIcon, Bars3Icon } from '@heroicons/react/24/outline';
+import { HomeIcon } from '@heroicons/react/20/solid';
 
 const Header = () => {
     return (
         <Fragment>
-            <div className='flex justify-between max-w-6xl'>
-                <div className='relative hidden lg:inline-grid h-24 w-24 mt-3'>
-                    <Image src='https://links.papareact.com/ocw' alt='full-logo-image' width={1000} height={1000} />
-                </div>
+            <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
+                <div className='flex justify-between max-w-6xl mx-5 xl:mx-auto my-3'>
+                    <div className='hidden lg:relative lg:flex h-10 w-24'>
+                        <Image src='https://links.papareact.com/ocw' alt='full-logo-image' width={1000} height={1000} />
+                    </div>
 
-                <div className='relative w-10 lg:hidden flex-shrink-0 cursor-pointer mt-3'>
-                    <Image src='https://links.papareact.com/jjm' alt='mini-logo-image' width={1000} height={1000} />
-                </div>
+                    <div className='relative w-10 lg:hidden flex-shrink-0 cursor-pointer'>
+                        <Image src='https://links.papareact.com/jjm' alt='mini-logo-image' width={1000} height={1000} />
+                    </div>
 
-                <div className='max-w-xs'>
-                    <div className='relative mt-1 p-3 rounded-md'>
-                        <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
-                            <MagnifyingGlassIcon className='h-5 w-5 text-gray-500' />
+                    <div className='max-w-xs'>
+                        <div className='relative rounded-md'>
+                            <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+                                <MagnifyingGlassIcon className='h-5 w-5 text-gray-500' />
+                            </div>
+                            <input className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md' type='text' placeholder='Search' />
                         </div>
-                        <input className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md' type='text' placeholder='Search' />
+                    </div>
+
+                    <div className='flex items-center justify-end space-x-4'>
+                        <HomeIcon className='navBtn' />
+                        <Bars3Icon className='h-6 md:hidden cursor-pointer' />
+                        <div className='relative navBtn'>
+                            <PaperAirplaneIcon className='navBtn rotate-315' />
+                            <div className='absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>3</div>
+                        </div>
+                        <PlusCircleIcon className='navBtn' />
+                        <UserGroupIcon className='navBtn' />
+                        <HeartIcon className='navBtn' />
+                        <Image src='https://avatars.githubusercontent.com/u/84142253?s=96&v=4' alt='profi-picture'
+                            className='h-10 w-10 rounded-full cursor-pointer' width={1000} height={1000} />
                     </div>
                 </div>
-
             </div>
         </Fragment>
     )
